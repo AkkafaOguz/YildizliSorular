d4d26cbc76e0d346147fb6a4f5e261fad1405342
Oguzhan <akkafaoguz@gmail.com>
2021-09-18 12:30:02 +0300

    updated

--- TekrarsizElemanYazdirma.java
+++ TekrarsizElemanYazdirma.java
@@ -1,12 +1,20 @@
 package YildizliSorular;
 
+import java.util.Arrays;
+
 public class TekrarsizElemanYazdirma {
 
 	public static void main(String[] args) {
 		
-        System.out.println(singleNumber(new int[]{2,2,1}));  // 1
+        //System.out.println(singleNumber(new int[]{2,2,1}));  // 1
         System.out.println(singleNumber(new int[]{4,1,2,1,2})); //4
-        System.out.println(singleNumber(new int[]{1})); //1
+        //System.out.println(singleNumber(new int[]{1})); //1
+        
+        System.out.println();
+        
+//        System.out.println(solution(new int[]{2,2,1}));  // 1
+//        System.out.println(solution(new int[]{4,1,2,1,2})); //4
+//        System.out.println(solution(new int[]{1})); //1
 
     }
     static int singleNumber(int[] array)
@@ -15,11 +23,19 @@ public class TekrarsizElemanYazdirma {
 
         for(int i=0;i<array.length;i++)
         {
-            result ^= array[i];
+        	System.out.println(result ^= array[i]); 
         }
 
         return result;
     }
+    
+static int solution(int[] numbers) {
+		
+		int distinctTotal = Arrays.stream(numbers).distinct().sum();
+		int arrayTotal = Arrays.stream(numbers).sum();
+		
+		return 2 * distinctTotal - arrayTotal;
+	}
 
 	}
 
